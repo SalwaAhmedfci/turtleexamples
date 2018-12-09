e1d0001cc8cb35a5361a2984d5d5685030ab4c52
SalwaAhmedfci <salwafci94@gmail.com>
2018-12-09 07:15:16 -0500

    adding the file

---
+++ turtleexamples.py
@@ -0,0 +1,14 @@
+import turtle
+window = turtle.Screen()
+
+def draw_circle():
+    angie = turtle.Turtle()
+    angie.color("blue")
+    angie.shape("turtle")
+    angie.speed(1)
+    angie.circle(100)
+
+
+
+draw_circle()
+window.exitonclick()
